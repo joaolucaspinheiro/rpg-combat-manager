83aa530dd845333737b67ffeac88b092d1f013ac
João Lucas Pinheiro <lucasdallas25@gmail.com>
2025-10-09 19:58:28 -0300

    Add XML configuration files and update Java entity imports for Jakarta Persistence

--- demo/src/main/java/com/poo/entity/EntidadeCombate.java
+++ demo/src/main/java/com/poo/entity/EntidadeCombate.java
@@ -1,13 +1,13 @@
 package com.poo.entity;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.OneToMany;
-import javax.persistence.CascadeType;
-import javax.persistence.FetchType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
 import java.util.List;
 
 @Entity
