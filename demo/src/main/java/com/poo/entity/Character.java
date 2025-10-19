4cb4ab317eb2b9e66a5c56bd5d4d0b2d2091e233
João Lucas Pinheiro <lucasdallas25@gmail.com>
2025-10-19 14:05:08 -0300

    Add characterClass and experiencePoints attributes to Character class; introduce EnemyType attribute to Enemy class

--- demo/src/main/java/com/poo/entity/Character.java
+++ demo/src/main/java/com/poo/entity/Character.java
@@ -4,6 +4,24 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Character extends CombatEntity {
+    // atributos especificos para o personagem
+    private String characterClass; // classe ex: guerreiro, mago, arqueiro
+    private int experiencePoints; // pontos de experiencia acumulados
+
+    // --- Getters and Setters ---
+    public String getCharacterClass() {
+        return characterClass;
+    }
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
+    public int getExperiencePoints() {
+        return experiencePoints;
+    }
+    public void setExperiencePoints(int experiencePoints) {
+        this.experiencePoints = experiencePoints;
+    }
+
 
 
 }
--- demo/src/main/java/com/poo/entity/Enemy.java
+++ demo/src/main/java/com/poo/entity/Enemy.java
@@ -4,4 +4,15 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Enemy extends CombatEntity {
+    // Tipo específico para Inimigo
+    private String EnemyType;
+
+
+    // --- Getters and Setters ---
+    public String getEnemyType() {
+        return EnemyType;
+    }
+    public void setEnemyType(String enemyType) {
+        EnemyType = enemyType;
+    }
 }
